d4410eb5fd629b807ba698e798f9b3fcf1dfc949
seroven <sebastianrodriguezventura@gmail.com>
2023-12-04 13:06:09 -0500

    setup project

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>
+        Hello world!
+    </h1>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+    mounted() {
+        this.test();
+    },
+    methods: {
+        async test() {
+            const responseTest = await axios.get('/test');
+            console.log(responseTest);
+        }
+    }
+}
+</script>
